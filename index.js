aca3119eaa11eecc42bb4307674f10615e9aa5d8
Will Cooley <william.cooley@gmail.coom>
2023-08-06 15:41:21 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(e){
+    let thing = document.getElementById("text");
+    thing.textContent = "This is really cool!";
+    console.log(e);
+})
